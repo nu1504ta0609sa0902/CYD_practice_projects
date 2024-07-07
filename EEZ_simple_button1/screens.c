d3df50da435c80d2cb0ad1475ac5c902d9d7b7cc
nu1504ta0609sa0902 <nooruddin1504@aol.com>
2024-07-07 15:39:37 +0100

    Test

---
+++ EEZ_simple_button1/screens.c
@@ -0,0 +1,66 @@
+#include "screens.h"
+#include "images.h"
+#include "fonts.h"
+#include "actions.h"
+#include "vars.h"
+#include "styles.h"
+#include "ui.h"
+
+objects_t objects;
+lv_obj_t *tick_value_change_obj;
+
+void create_screen_main() {
+    lv_obj_t *obj = lv_obj_create(0);
+    objects.main = obj;
+    lv_obj_set_pos(obj, 0, 0);
+    lv_obj_set_size(obj, 320, 240);
+    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+    {
+        lv_obj_t *parent_obj = obj;
+        {
+            lv_obj_t *obj = lv_label_create(parent_obj);
+            lv_obj_set_pos(obj, 356, 232);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_label_set_text(obj, "Hello, world!");
+        }
+        {
+            // main_btn1
+            lv_obj_t *obj = lv_btn_create(parent_obj);
+            objects.main_btn1 = obj;
+            lv_obj_set_pos(obj, 57, 95);
+            lv_obj_set_size(obj, 206, 50);
+            lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
+            lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+        }
+        {
+            // main_label1
+            lv_obj_t *obj = lv_label_create(parent_obj);
+            objects.main_label1 = obj;
+            lv_obj_set_pos(obj, 130, 112);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_label_set_text(obj, "Button 1");
+        }
+    }
+}
+
+void tick_screen_main() {
+}
+
+
+void create_screens() {
+    lv_disp_t *dispp = lv_disp_get_default();
+    lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), false, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
+    
+    create_screen_main();
+}
+
+typedef void (*tick_screen_func_t)();
+
+tick_screen_func_t tick_screen_funcs[] = {
+    tick_screen_main,
+};
+
+void tick_screen(int screen_index) {
+    tick_screen_funcs[screen_index]();
+}
