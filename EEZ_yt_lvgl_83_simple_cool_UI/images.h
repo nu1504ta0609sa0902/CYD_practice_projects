6601067c2bbfc5d78863da9bbf85e265dde15715
nu1504ta0609sa0902 <nooruddin1504@aol.com>
2024-07-13 18:37:54 +0100

    How to generate cool effect with image with slider

---
+++ EEZ_yt_lvgl_83_simple_cool_UI/images.h
@@ -0,0 +1,28 @@
+#ifndef EEZ_LVGL_UI_IMAGES_H
+#define EEZ_LVGL_UI_IMAGES_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_img_dsc_t img__bg_img;
+extern const lv_img_dsc_t img__fg_img;
+
+#ifndef EXT_IMG_DESC_T
+#define EXT_IMG_DESC_T
+typedef struct _ext_img_desc_t {
+    const char *name;
+    const lv_img_dsc_t *img_dsc;
+} ext_img_desc_t;
+#endif
+
+extern const ext_img_desc_t images[2];
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_IMAGES_H*/
