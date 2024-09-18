e2b9b975667fc37b948b3fbe82148db8cf6c7c34
nu1504ta0609sa0902 <nooruddin1504@aol.com>
2024-09-18 19:58:08 +0100

    How to add Bitmap Images and use them

---
+++ LVGL9/EEZ_add_images_and_special_characters/screens.c
@@ -0,0 +1,65 @@
+#include <string.h>
+
+#include "screens.h"
+#include "images.h"
+#include "fonts.h"
+#include "actions.h"
+#include "vars.h"
+#include "styles.h"
+#include "ui.h"
+
+#include <string.h>
+
+objects_t objects;
+lv_obj_t *tick_value_change_obj;
+
+void create_screen_main() {
+    lv_obj_t *obj = lv_obj_create(0);
+    objects.main = obj;
+    lv_obj_set_pos(obj, 0, 0);
+    lv_obj_set_size(obj, 320, 240);
+    lv_obj_set_style_bg_color(obj, lv_color_hex(0xff000000), LV_PART_MAIN | LV_STATE_DEFAULT);
+    {
+        lv_obj_t *parent_obj = obj;
+        {
+            // imageNeedle
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            objects.image_needle = obj;
+            lv_obj_set_pos(obj, 75, 19);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_img_set_src(obj, &img_needle);
+        }
+        {
+            // labelValue
+            lv_obj_t *obj = lv_label_create(parent_obj);
+            objects.label_value = obj;
+            lv_obj_set_pos(obj, 108, 189);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_label_set_text(obj, "£ GBP");
+            lv_obj_set_style_text_color(obj, lv_color_hex(0xffff0000), LV_PART_MAIN | LV_STATE_DEFAULT);
+            lv_obj_set_style_text_font(obj, &ui_font_orbitron_30, LV_PART_MAIN | LV_STATE_DEFAULT);
+        }
+    }
+}
+
+void tick_screen_main() {
+}
+
+
+void create_screens() {
+    lv_disp_t *dispp = lv_disp_get_default();
+    lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), false, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
+    
+    create_screen_main();
+}
+
+typedef void (*tick_screen_func_t)();
+
+tick_screen_func_t tick_screen_funcs[] = {
+    tick_screen_main,
+};
+
+void tick_screen(int screen_index) {
+    tick_screen_funcs[screen_index]();
+}
